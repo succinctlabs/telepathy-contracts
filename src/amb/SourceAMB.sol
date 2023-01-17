b72a395bfdfb43e31926b434bee0204e7ab79207
John Guibas <jtguibas@gmail.com>
2023-01-17 11:00:20 -0800

    Add Natspec comments to all contracts (#72)
    * Commented AMBs
    
    * Natspec comments for lightclient
    
    * Fix some multiline issues
    
    * Weird forge fmt behavior
    
    GitOrigin-RevId: 4e9ade477120af59fc10919b24f3f4262e558e14

--- src/amb/SourceAMB.sol
+++ src/amb/SourceAMB.sol
@@ -3,15 +3,22 @@ pragma experimental ABIEncoderV2;
 
 import "src/amb/interfaces/IAMB.sol";
 
+/// @title Source Arbitrary Message Bridge
+/// @author Succinct Labs
+/// @notice This contract is the entrypoint for making a cross-chain call.
 contract SourceAMB is IBroadcaster {
+    /// @notice Mapping between a nonce and a message root.
     mapping(uint256 => bytes32) public messages;
-    uint256 public nonce;
 
-    constructor() {
-        nonce = 1; // We initialize with 1 to get accurate gas numbers during testing
-            // since changing a slot from 0 is different the changing it from any other number.
-    }
+    /// @notice Keeps track of the next nonce to be used.
+    uint256 public nonce = 1;
 
+    /// @notice Sends a message to a target chain.
+    /// @param recipient The contract address that will be called on the target chain.
+    /// @param recipientChainId The chain id that specifies the target chain.
+    /// @param gasLimit The maximum amount of gas the call on the target chain is allowed to take.
+    /// @param data The calldata used when calling the contract on the target chain.
+    /// @return bytes32 A unique identifier for a message.
     function send(address recipient, uint16 recipientChainId, uint256 gasLimit, bytes calldata data)
         external
         returns (bytes32)
@@ -24,13 +31,18 @@ contract SourceAMB is IBroadcaster {
         return messageRoot;
     }
 
+    /// @notice Sends a message to a target chain using an event proof instead of a storage proof.
+    /// @param recipient The contract address that will be called on the target chain.
+    /// @param recipientChainId The chain id that specifies the target chain.
+    /// @param gasLimit The maximum amount of gas the call on the target chain is allowed to take.
+    /// @param data The calldata used when calling the contract on the target chain.
+    /// @return bytes32 A unique identifier for a message.
     function sendViaLog(
         address recipient,
         uint16 recipientChainId,
         uint256 gasLimit,
         bytes calldata data
     ) external returns (bytes32) {
-        // Heavily gas optimized
         bytes memory message =
             abi.encode(nonce, msg.sender, recipient, recipientChainId, gasLimit, data);
         bytes32 messageRoot = keccak256(message);
