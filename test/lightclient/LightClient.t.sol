2d1ac3f948aad11a792ce97dd59a74b741c6762a
Matt Stam <mattstam@live.com>
2023-02-23 13:37:31 -0800

    generate LightClient fixtures and read in test (#155)
    GitOrigin-RevId: b08b1dbfed37b93bc24efca448dddd3fee206171

--- test/lightclient/LightClient.t.sol
+++ test/lightclient/LightClient.t.sol
@@ -7,69 +7,113 @@ import "forge-std/Test.sol";
 import {SSZ} from "src/libraries/SimpleSerialize.sol";
 import {LightClient, LightClientStep, LightClientRotate} from "src/lightclient/LightClient.sol";
 import {LightClientFixture} from "test/lightclient/LightClientFixture.sol";
+import {Strings} from "openzeppelin-contracts/utils/Strings.sol";
 
 contract LightClientTest is Test, LightClientFixture {
     uint32 constant SOURCE_CHAIN_ID = 1;
     uint16 constant FINALITY_THRESHOLD = 350;
 
+    uint256 constant FIXTURE_SLOT_START = 5043198;
+    uint256 constant FIXTURE_SLOT_END = 5053982;
+
+    Fixture[] fixtures;
+
     function setUp() public {
+        // read all fixtures from entire directory
+        string memory root = vm.projectRoot();
+        for (uint256 i = 0; i < FIXTURE_SLOT_END - FIXTURE_SLOT_START; i++) {
+            uint256 slot = FIXTURE_SLOT_START + i;
+
+            string memory filename = string.concat("slot", Strings.toString(slot));
+            string memory path =
+                string.concat(root, "/test/lightclient/fixtures/", filename, ".json");
+            try vm.readFile(path) returns (string memory file) {
+                bytes memory parsed = vm.parseJson(file);
+                Fixture memory params = abi.decode(parsed, (Fixture));
+                fixtures.push(params);
+            } catch {
+                continue;
+            }
+        }
+
         vm.warp(9999999999999);
     }
 
+    function testSetup() public view {
+        require(fixtures.length > 0, "no fixtures found");
+    }
+
     function testStep() public {
-        Fixture memory fixture = parseFixture("lcUpdate");
+        for (uint256 i = 0; i < fixtures.length; i++) {
+            Fixture memory fixture = fixtures[i];
 
-        LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
-        LightClientStep memory step = convertToLightClientStep(fixture.step);
+            LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
+            LightClientStep memory step = convertToLightClientStep(fixture.step);
 
-        lc.step(step);
+            lc.step(step);
+        }
     }
 
     function testRotate() public {
-        Fixture memory fixture = parseFixture("lcUpdate");
+        for (uint256 i = 0; i < fixtures.length; i++) {
+            Fixture memory fixture = fixtures[i];
 
-        LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
-        LightClientRotate memory rotate = convertToLightClientRotate(fixture.step, fixture.rotate);
+            LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
+            LightClientRotate memory rotate =
+                convertToLightClientRotate(fixture.step, fixture.rotate);
 
-        lc.rotate(rotate);
+            lc.rotate(rotate);
+        }
     }
 
-    function testRawProof() public {
-        Fixture memory fixture = LightClientFixture.parseFixture("lcUpdate");
+    function testRawStepProof() public {
+        for (uint256 i = 0; i < fixtures.length; i++) {
+            Fixture memory fixture = fixtures[i];
 
-        LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
+            LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
 
-        uint256[2] memory a = [strToUint(fixture.step.a[0]), strToUint(fixture.step.a[1])];
-        uint256[2][2] memory b = [
-            [strToUint(fixture.step.b[0][1]), strToUint(fixture.step.b[0][0])],
-            [strToUint(fixture.step.b[1][1]), strToUint(fixture.step.b[1][0])]
-        ];
-        uint256[2] memory c = [strToUint(fixture.step.c[0]), strToUint(fixture.step.c[1])];
-        uint256[1] memory inputs = [strToUint(fixture.step.inputs[0])];
+            uint256[2] memory a = [strToUint(fixture.step.a[0]), strToUint(fixture.step.a[1])];
+            uint256[2][2] memory b = [
+                [strToUint(fixture.step.b[0][1]), strToUint(fixture.step.b[0][0])],
+                [strToUint(fixture.step.b[1][1]), strToUint(fixture.step.b[1][0])]
+            ];
+            uint256[2] memory c = [strToUint(fixture.step.c[0]), strToUint(fixture.step.c[1])];
+            uint256[1] memory inputs = [strToUint(fixture.step.inputs[0])];
 
-        require(lc.verifyProofStep(a, b, c, inputs) == true);
+            require(lc.verifyProofStep(a, b, c, inputs) == true);
+        }
     }
 
-    function testPublicInputsRoot() public view {
-        bytes32 finalizedHeaderRoot =
-            bytes32(0xfad3ba4e53d01e392d6c191294d6191f51d769666bae2869ca3d2ac962c2cace);
-        bytes32 finalizedSlotLE = SSZ.toLittleEndian(4359712);
-        bytes32 participationLE = SSZ.toLittleEndian(426);
-        bytes32 executionStateRoot =
-            bytes32(0x6fc15b26deadfb27063a8ee1147dd66e060ddea457a7b9bb4473199dc352ab47);
-        bytes32 syncCommitteePoseidon = SSZ.toLittleEndian(
-            7032059424740925146199071046477651269705772793323287102921912953216115444414
-        );
+    function testRawRotateProof() public {
+        for (uint256 i = 0; i < fixtures.length; i++) {
+            Fixture memory fixture = fixtures[i];
 
-        bytes32 h;
-        h = sha256(bytes.concat(finalizedSlotLE, finalizedHeaderRoot));
-        console.logBytes32(h);
-        h = sha256(bytes.concat(h, participationLE));
-        console.logBytes32(h);
-        h = sha256(bytes.concat(h, executionStateRoot));
-        console.logBytes32(h);
-        h = sha256(bytes.concat(h, syncCommitteePoseidon));
-        console.logBytes32(h);
-        console.logUint(uint256(h) & (uint256(1 << 253) - 1));
+            LightClient lc = newLightClient(fixture.initial, SOURCE_CHAIN_ID, FINALITY_THRESHOLD);
+
+            uint256[2] memory a = [strToUint(fixture.rotate.a[0]), strToUint(fixture.rotate.a[1])];
+            uint256[2][2] memory b = [
+                [strToUint(fixture.rotate.b[0][1]), strToUint(fixture.rotate.b[0][0])],
+                [strToUint(fixture.rotate.b[1][1]), strToUint(fixture.rotate.b[1][0])]
+            ];
+            uint256[2] memory c = [strToUint(fixture.rotate.c[0]), strToUint(fixture.rotate.c[1])];
+
+            LightClientRotate memory rotate =
+                convertToLightClientRotate(fixture.step, fixture.rotate);
+
+            uint256[65] memory inputs;
+            uint256 syncCommitteeSSZNumeric = uint256(rotate.syncCommitteeSSZ);
+            for (uint256 i = 0; i < 32; i++) {
+                inputs[32 - 1 - i] = syncCommitteeSSZNumeric % 2 ** 8;
+                syncCommitteeSSZNumeric = syncCommitteeSSZNumeric / 2 ** 8;
+            }
+            uint256 finalizedHeaderRootNumeric = uint256(fixture.step.finalizedHeaderRoot);
+            for (uint256 i = 0; i < 32; i++) {
+                inputs[64 - i] = finalizedHeaderRootNumeric % 2 ** 8;
+                finalizedHeaderRootNumeric = finalizedHeaderRootNumeric / 2 ** 8;
+            }
+            inputs[32] = uint256(SSZ.toLittleEndian(uint256(rotate.syncCommitteePoseidon)));
+
+            require(lc.verifyProofRotate(a, b, c, inputs) == true);
+        }
     }
 }
