0e762be431a8cbbc875729d580706a66063adc29
Matt Stam <mattstam@live.com>
2023-02-27 11:32:59 -0800

    add Message encoding fuzz testing (#169)
    GitOrigin-RevId: 2b95a43cacb37a9e42c8d48624eb0cacff9318d6

--- test/libraries/MessageEncoding.t.sol
+++ test/libraries/MessageEncoding.t.sol
@@ -7,52 +7,137 @@ import {Message} from "src/amb/interfaces/ITelepathy.sol";
 import {MessageEncoding} from "src/libraries/MessageEncoding.sol";
 
 contract MessageEncodingTest is Test {
-    function setUp() public {}
+    uint8 constant DEFAULT_VERSION = 1;
+    uint64 constant DEFAULT_NONCE = 123;
+    uint32 constant DEFAULT_SOURCE_CHAIN_ID = 5;
+    address constant DEFAULT_SENDER_ADDRESS = address(0xe2B19845Fe2B7Bb353f377d12dD51af012fbba20);
+    uint32 constant DEFAULT_RECIPIENT_CHAIN_ID = 100;
+    bytes32 constant DEFAULT_RECIPIENT_ADDRESS = bytes32(uint256(12345678));
+    bytes constant DEFAULT_DATA = hex"6789";
 
-    // TODO add fuzz tests here for the message encoding
-    function testMessageEncoding() public view {
-        Message memory message = Message({
-            version: 1,
-            nonce: 123,
-            sourceChainId: 5,
-            senderAddress: address(0xe2B19845Fe2B7Bb353f377d12dD51af012fbba20),
-            recipientChainId: 100,
-            recipientAddress: bytes32(uint256(12345678)),
-            data: hex"6789"
-        });
-        bytes memory msgBytes = MessageEncoding.encode(message);
-        console.logBytes(msgBytes);
-        console.logUint(msgBytes.length);
-        Message memory decoded = MessageEncoding.decode(msgBytes);
-        require(decoded.version == message.version);
-        require(decoded.nonce == message.nonce);
-        require(decoded.sourceChainId == message.sourceChainId);
-        require(decoded.senderAddress == message.senderAddress);
-        require(decoded.recipientChainId == message.recipientChainId);
-        require(decoded.recipientAddress == message.recipientAddress);
-        require(keccak256(decoded.data) == keccak256(message.data));
+    function equals(Message memory a, Message memory b) internal pure returns (bool) {
+        return a.version == b.version && a.nonce == b.nonce && a.sourceChainId == b.sourceChainId
+            && a.senderAddress == b.senderAddress && a.recipientChainId == b.recipientChainId
+            && a.recipientAddress == b.recipientAddress && keccak256(a.data) == keccak256(b.data);
     }
 
-    function testMessageEncodingLong() public view {
-        console.log("testMessage");
-        Message memory message = Message({
-            version: 1,
-            nonce: 123,
-            sourceChainId: 5,
-            senderAddress: address(0xe2B19845Fe2B7Bb353f377d12dD51af012fbba20),
-            recipientChainId: 100,
-            recipientAddress: bytes32(uint256(12345678)),
-            data: hex"6789"
+    function test_MessageEncoding() public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: DEFAULT_DATA
         });
-        bytes memory msgBytes = abi.encode(message);
-        console.logUint(msgBytes.length);
-        Message memory decoded = abi.decode(msgBytes, (Message));
-        require(decoded.version == message.version);
-        require(decoded.nonce == message.nonce);
-        require(decoded.sourceChainId == message.sourceChainId);
-        require(decoded.senderAddress == message.senderAddress);
-        require(decoded.recipientChainId == message.recipientChainId);
-        require(decoded.recipientAddress == message.recipientAddress);
-        require(keccak256(decoded.data) == keccak256(message.data));
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_Version(uint8 version) public pure {
+        Message memory original = Message({
+            version: version,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: DEFAULT_DATA
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_Nonce(uint64 nonce) public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: nonce,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: DEFAULT_DATA
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_SourceChainId(uint32 sourceChainId) public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: sourceChainId,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: DEFAULT_DATA
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_SenderAddress(address senderAddress) public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: senderAddress,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: DEFAULT_DATA
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_RecipientChainId(uint32 recipientChainId) public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: recipientChainId,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: DEFAULT_DATA
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_RecipientAddress(bytes32 recipientAddress) public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: recipientAddress,
+            data: DEFAULT_DATA
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
+    }
+
+    function testFuzz_MessageEncoding_Data(bytes memory data) public pure {
+        Message memory original = Message({
+            version: DEFAULT_VERSION,
+            nonce: DEFAULT_NONCE,
+            sourceChainId: DEFAULT_SOURCE_CHAIN_ID,
+            senderAddress: DEFAULT_SENDER_ADDRESS,
+            recipientChainId: DEFAULT_RECIPIENT_CHAIN_ID,
+            recipientAddress: DEFAULT_RECIPIENT_ADDRESS,
+            data: data
+        });
+        bytes memory msgBytes = MessageEncoding.encode(original);
+        Message memory decoded = MessageEncoding.decode(msgBytes);
+        require(equals(decoded, original));
     }
 }
