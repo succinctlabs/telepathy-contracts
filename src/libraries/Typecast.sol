095a4ff80552bba979e88d3b00dbd3a801987568
Matt Stam <mattstam@live.com>
2023-02-27 16:36:23 -0800

    add GH action for solhint and contract test coverage (#179)
    GitOrigin-RevId: 7bf9ca45966334158daa7a6f2a4a97792fb60288

--- src/libraries/Typecast.sol
+++ src/libraries/Typecast.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT OR Apache-2.0
-pragma solidity >=0.6.11;
+pragma solidity 0.8.16;
 
 library Address {
     function fromBytes32(bytes32 buffer) internal pure returns (address) {
