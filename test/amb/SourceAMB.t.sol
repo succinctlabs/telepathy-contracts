194ac1fe3291a69754e4a74b362dcd814802d3bc
Matt Stam <mattstam@live.com>
2023-02-28 14:19:29 -0800

    add sourceAMB test coverage (#188)
    GitOrigin-RevId: 31d2b6612d0c2a39e112f8e7fa915f75309cf8cd

--- test/amb/SourceAMB.t.sol
+++ test/amb/SourceAMB.t.sol
@@ -3,14 +3,24 @@ pragma solidity 0.8.16;
 import "forge-std/Vm.sol";
 import "forge-std/console.sol";
 import "forge-std/Test.sol";
-
-import {TelepathyRouter} from "src/amb/TelepathyRouter.sol";
-import {UUPSProxy} from "src/libraries/Proxy.sol";
-
 import {WrappedInitialize} from "./TargetAMB.t.sol";
+import {TelepathyRouter} from "src/amb/TelepathyRouter.sol";
+import {SourceAMB} from "src/amb/SourceAMB.sol";
+import {UUPSProxy} from "src/libraries/Proxy.sol";
+import {Bytes32} from "src/libraries/Typecast.sol";
+import {MessageEncoding} from "src/libraries/MessageEncoding.sol";
 
 contract SourceAMBTest is Test {
+    event SentMessage(uint64 indexed nonce, bytes32 indexed msgHash, bytes message);
+
+    uint32 constant DEFAULT_RECIPIENT_CHAIN_ID = 100;
+    address constant DEFAULT_RECIPIENT_ADDR = 0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990;
+    bytes32 constant DEFAULT_RECIPIENT_ADDR_BYTES32 = bytes32("0x690B9A9E9aa1C9dB991C7721a92d35");
+    bytes constant DEFAULT_RECIPIENT_DATA = hex"deadbeef";
+
     TelepathyRouter wrappedSourceAMBProxy;
+
+    address bob = payable(makeAddr("bob"));
 
     function setUp() public {
         TelepathyRouter sourceAMBImplementation = new TelepathyRouter();
@@ -19,7 +29,7 @@ contract SourceAMBTest is Test {
         wrappedSourceAMBProxy = TelepathyRouter(address(proxy));
         WrappedInitialize.init(
             address(wrappedSourceAMBProxy),
-            1,
+            uint32(block.chainid),
             makeAddr("lightclient"),
             makeAddr("sourceAMB"),
             address(this),
@@ -27,11 +37,225 @@ contract SourceAMBTest is Test {
         );
     }
 
-    function testSend() public {
-        wrappedSourceAMBProxy.send(100, 0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990, hex"deadbeef");
+    function test_Send_WhenAddressRecipient() public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            Bytes32.fromAddress(DEFAULT_RECIPIENT_ADDR),
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.send(
+            DEFAULT_RECIPIENT_CHAIN_ID, DEFAULT_RECIPIENT_ADDR, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
     }
 
-    function testSendViaLog() public {
-        wrappedSourceAMBProxy.send(100, 0x690B9A9E9aa1C9dB991C7721a92d351Db4FaC990, hex"deadbeef");
+    function test_Send_WhenBytes32Recipient() public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            DEFAULT_RECIPIENT_ADDR_BYTES32,
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.send(
+            DEFAULT_RECIPIENT_CHAIN_ID, DEFAULT_RECIPIENT_ADDR_BYTES32, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
+    }
+
+    function test_SendViaStorage_WhenAddressRecipient() public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            Bytes32.fromAddress(DEFAULT_RECIPIENT_ADDR),
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.sendViaStorage(
+            DEFAULT_RECIPIENT_CHAIN_ID, DEFAULT_RECIPIENT_ADDR, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
+        assertEq(
+            wrappedSourceAMBProxy.messages(SourceAMB(wrappedSourceAMBProxy).nonce() - 1),
+            expectedMessageRoot
+        );
+    }
+
+    function test_SendViaStorage_WhenBytes32Recipient() public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            DEFAULT_RECIPIENT_ADDR_BYTES32,
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.sendViaStorage(
+            DEFAULT_RECIPIENT_CHAIN_ID, DEFAULT_RECIPIENT_ADDR_BYTES32, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
+        assertEq(
+            wrappedSourceAMBProxy.messages(SourceAMB(wrappedSourceAMBProxy).nonce() - 1),
+            expectedMessageRoot
+        );
+    }
+
+    function testFuzz_Send_MsgSender(address sender) public {
+        vm.startPrank(sender);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            sender,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            Bytes32.fromAddress(DEFAULT_RECIPIENT_ADDR),
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.send(
+            DEFAULT_RECIPIENT_CHAIN_ID, DEFAULT_RECIPIENT_ADDR, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
+    }
+
+    function testFuzz_Send_RecipientChainId(uint32 _chainId) public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            _chainId,
+            Bytes32.fromAddress(DEFAULT_RECIPIENT_ADDR),
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot =
+            wrappedSourceAMBProxy.send(_chainId, DEFAULT_RECIPIENT_ADDR, DEFAULT_RECIPIENT_DATA);
+        assertEq(messageRoot, expectedMessageRoot);
+    }
+
+    function testFuzz_Send_RecipientAddress(address _recipientAddress) public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            Bytes32.fromAddress(_recipientAddress),
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.send(
+            DEFAULT_RECIPIENT_CHAIN_ID, _recipientAddress, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
+    }
+
+    function testFuzz_Send_RecipientBytes32(bytes32 _recipientAddress) public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            _recipientAddress,
+            DEFAULT_RECIPIENT_DATA
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot = wrappedSourceAMBProxy.send(
+            DEFAULT_RECIPIENT_CHAIN_ID, _recipientAddress, DEFAULT_RECIPIENT_DATA
+        );
+        assertEq(messageRoot, expectedMessageRoot);
+    }
+
+    function testFuzz_Send_Data(bytes calldata _data) public {
+        vm.startPrank(bob);
+        bytes memory expectedMessage = MessageEncoding.encode(
+            wrappedSourceAMBProxy.VERSION(),
+            SourceAMB(wrappedSourceAMBProxy).nonce(),
+            uint32(block.chainid),
+            bob,
+            DEFAULT_RECIPIENT_CHAIN_ID,
+            Bytes32.fromAddress(DEFAULT_RECIPIENT_ADDR),
+            _data
+        );
+        bytes32 expectedMessageRoot = keccak256(expectedMessage);
+
+        vm.expectEmit(true, true, true, true);
+        emit SentMessage(
+            SourceAMB(wrappedSourceAMBProxy).nonce(), expectedMessageRoot, expectedMessage
+        );
+
+        bytes32 messageRoot =
+            wrappedSourceAMBProxy.send(DEFAULT_RECIPIENT_CHAIN_ID, DEFAULT_RECIPIENT_ADDR, _data);
+        assertEq(messageRoot, expectedMessageRoot);
     }
 }
