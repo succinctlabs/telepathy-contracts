bd0f2c91048268c680ef9a750f068a493d5d54b0
Siddhant Sharma <47306418+siddhantsharma301@users.noreply.github.com>
2023-03-08 17:54:44 -0800

    Example `Counter` updates (#241)
    * My changes
    
    * fix forge fmt issues:
    
    * counter example fixed up
    
    * lint up example
    
    * add unchecked math for overflowooooors
    
    * lint
    
    * rename increment overflow test
    
    * update for no ownership
    
    * fix example counter to not have unnecessary loc
    
    * remove events and other stuff
    
    * fmt fix
    
    ---------
    
    Co-authored-by: John Guibas <jtguibas@gmail.com>
    GitOrigin-RevId: 0ebccea9dfb0873294de3f24adff0a9395c7811b

--- examples/example-counter/ExampleCounter.sol
+++ examples/example-counter/ExampleCounter.sol
@@ -1,15 +1,11 @@
 pragma solidity 0.8.16;
 
-// import "openzeppelin-contracts/access/Ownable.sol";
 import {ITelepathyRouter} from "src/amb/interfaces/ITelepathy.sol";
 import {TelepathyHandler} from "src/amb/interfaces/TelepathyHandler.sol";
 
 contract SourceCounter {
     ITelepathyRouter router;
     uint32 targetChainId;
-
-    // Useful for indexing on subgraph
-    event SourceIncrement(uint32 targetChainId, address indexed from, uint256 amount);
 
     constructor(address _router, uint32 _targetChainId) {
         router = ITelepathyRouter(_router);
@@ -20,31 +16,22 @@ contract SourceCounter {
     function increment(uint256 amount, address targetCounter) external virtual {
         bytes memory msgData = abi.encode(amount);
         router.send(targetChainId, targetCounter, msgData);
-        emit SourceIncrement(targetChainId, msg.sender, amount);
     }
 }
 
 contract TargetCounter is TelepathyHandler {
     uint256 public counter = 0;
-    TelepathyHandler router;
 
-    // Useful for indexing on subgraph
-    event TargetIncrement(uint32 indexed sourceChainId, address indexed sender, uint256 amount);
-
-    constructor(address _router) TelepathyHandler(_router) {
-        router = TelepathyHandler(_router);
-    }
+    constructor(address _router) TelepathyHandler(_router) {}
 
     // Handle messages being sent and decoding
     function handleTelepathyImpl(uint32 sourceChainId, address sender, bytes memory msgData)
         internal
         override
     {
-        require(msg.sender == address(router), "Sender is not router");
         (uint256 amount) = abi.decode(msgData, (uint256));
         unchecked {
             counter = counter + amount;
         }
-        emit TargetIncrement(sourceChainId, sender, amount);
     }
 }
